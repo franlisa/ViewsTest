08249c605b5994e7449353002c1f76be2bb7372b
xuejiao.fxj <xuejiao.fxj@alibaba-inc.com>
2015-10-15 16:34:03 +0800

    回退处理

--- app/src/main/java/com/example/xuejiao/codepathtest/WebViewActivity.java
+++ app/src/main/java/com/example/xuejiao/codepathtest/WebViewActivity.java
@@ -1,7 +1,10 @@
 package com.example.xuejiao.codepathtest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -25,16 +28,32 @@ public class WebViewActivity extends Activity {
           如果页面中链接,如果希望点击链接继续在当前browser中响应,而不是新开Android的系统browser中响应该链接,必须覆盖 WebView的WebViewClient对象.
           所以如果将下面这段代码去掉，则页面中有链接，点击就会到浏览器打开
        */
-        mWvNormal.setWebViewClient(new WebViewClient(){
+        mWvNormal.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
+                //指定只有含有guide的时候才在当前webView中打开，否则还是启动浏览器打开
+                if (url.contains("guide")) {
+                    view.loadUrl(url);
+                    return true;
+                } else {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+//                return  super.shouldOverrideUrlLoading(view,url);
             }
-
         });
     }
     private  void initViews(){
         mWvNormal =(WebView)findViewById(R.id.wv_normal);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode == KeyEvent.KEYCODE_BACK) && mWvNormal.canGoBack()){
+            mWvNormal.goBack();
+            return  true;
+        }
+        return  super.onKeyDown(keyCode,event);
     }
 }
