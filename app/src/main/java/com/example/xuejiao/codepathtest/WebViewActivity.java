e846ad228b3329e3614d6de1f3a3b17958a3c4b9
xuejiao.fxj <xuejiao.fxj@alibaba-inc.com>
2015-10-15 16:09:49 +0800

    添加webClient 让页面点击的链接在当前browser打开

--- app/src/main/java/com/example/xuejiao/codepathtest/WebViewActivity.java
+++ app/src/main/java/com/example/xuejiao/codepathtest/WebViewActivity.java
@@ -3,6 +3,7 @@ package com.example.xuejiao.codepathtest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by xuejiao on 15/10/15.
@@ -14,12 +15,24 @@ public class WebViewActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.webview_layout);
         initViews();
-        mWvNormal.loadUrl("http://kymjs.com/code/2015/05/03/01/");
+//        mWvNormal.loadUrl("http://kymjs.com/code/2015/05/03/01/");
+        mWvNormal.loadUrl("http://guides.codepath.com/android/Working-with-the-WebView");
 
         mWvNormal.getSettings().setJavaScriptEnabled(true);
         mWvNormal.getSettings().setSupportZoom(false);
         mWvNormal.getSettings().setBuiltInZoomControls(true);
+        /*
+          如果页面中链接,如果希望点击链接继续在当前browser中响应,而不是新开Android的系统browser中响应该链接,必须覆盖 WebView的WebViewClient对象.
+          所以如果将下面这段代码去掉，则页面中有链接，点击就会到浏览器打开
+       */
+        mWvNormal.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
 
+        });
     }
     private  void initViews(){
         mWvNormal =(WebView)findViewById(R.id.wv_normal);
